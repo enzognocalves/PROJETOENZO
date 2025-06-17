77d4e104588a180da608347cae1002521fa9cbe0
enzognocalves <142853857+enzognocalves@users.noreply.github.com>
2025-06-17 11:04:00 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -11,15 +11,14 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def ler_dados_excel(caminho_arquivo):
     df = pd.read_excel(caminho_arquivo)
 
-    nomes_alunos = df["NOME DO ALUNO"].tolist()
-    nomes_professores = df["NOME DO PROFESSOR"].tolist()
-    atividades = df["ATIVIDADE"].tolist()
-    disciplinas = df["DISCIPLINA"].tolist()
-    emails = df["EMAIL DO ALUNO"].tolist()
-    datas_limite = df["DATA LIMITE"].tolist()
-    entregues = df["ENTREGUE"].tolist()
+    nomes_alunos = df["Nome_Completo"].tolist()
+    emails = df["Email"].tolist()
+    inicio_atividade = df["Iniciado_em"].tolist()
+    completo = df["Completo"].tolist()
+    nota = df["Nota"].tolist()
+    estado = df["Estado"].tolist()
 
-    return nomes_alunos, nomes_professores, atividades, disciplinas, emails, datas_limite, entregues
+    return nomes_alunos, emails, inicio_atividade, estado, completo, nota
 
 # Função para enviar e-mails
 def enviar_email(destinatario, assunto, corpo, remetente, senha):
@@ -44,16 +43,14 @@ def usar_dados():
     remetente = "enzogville8@gmail.com"
     senha = "zmjigrqcufahvxul" # Substitua por sua senha de app do Gmail
 
-    nomes_alunos, nomes_professores, atividades, disciplinas, emails, datas_limite, entregues = ler_dados_excel(caminho)
+    nomes_alunos, emails, inicio_atividade, estado, completo, nota = ler_dados_excel(caminho)
 
     for i in range(len(nomes_alunos)):
-        if str(entregues[i]).strip().upper() == "FALSE":
+        if str(estado[i]).strip().upper() != "Finalizada":
             corpo_email = (
                 f"Olá {nomes_alunos[i]},\n\n"
-                f"Você ainda não entregou a atividade: {atividades[i]}\n"
-                f"Disciplina: {disciplinas[i]}\n"
-                f"Professor: {nomes_professores[i]}\n"
-                f"Data limite para entrega: {datas_limite[i]}\n\n"
+                f"Você ainda não entregou a atividade"
+                f"Materia se deu inicio em: {inicio_atividade[i]}\n\n"
                 "Por favor, realize a entrega o quanto antes até a hora 23:59 da data vigente.\n\n"
                 "Atenciosamente,\n"
                 "Coordenação"
